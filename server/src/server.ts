a0eaa564f13f6920745412167c0fb18d9a9cc7d1
alnmaurofranco <jefeson23@live.com>
2022-11-07 20:48:14 -0300

    ♻️ refactor: addenv and jsonwebtoken

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import env from "@fastify/env";
 import Fastify from "fastify";
 import authRoutes from "./routes/auth.routes";
 import gameRoutes from "./routes/game.routes";
@@ -10,8 +12,24 @@ async function bootstrap() {
   const app = Fastify({
     logger: true,
   });
+  await app.register(env, {
+    dotenv: true,
+    schema: {
+      type: "object",
+      required: ["PORT"],
+      properties: {
+        PORT: {
+          type: "number",
+          default: 3000,
+        },
+      },
+    },
+  });
   await app.register(cors, {
     origin: true,
+  });
+  await app.register(jwt, {
+    secret: String(process.env.JWT_KEY_SECRET),
   });
   await app.register(authRoutes);
   await app.register(pollRoutes);
